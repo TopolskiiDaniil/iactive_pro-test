18d3f4256f5298ddef84331096b0c97b46213a40
TopolskiiDaniil <cool.topolskiy@gmail.com>
2025-10-10 18:38:54 +0300

    proxy update v5

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// export default defineConfig(({ command }) => ({
-//   plugins: [react()],
-//   server: command === 'serve' ? {
-//     proxy: {
-//       '/api': {
-//         target: 'http://a0830433.xsph.ru',
-//         changeOrigin: true,
-//         rewrite: (path) => path.replace(/^\/api/, ''),
-//       },
-//     },
-//   } : undefined,
-// }))
-export default defineConfig({
+// https://vitejs.dev/config/
+export default defineConfig(({ command }) => ({
   plugins: [react()],
-  server: {
+  server: command === 'serve' ? {
     proxy: {
       '/api': {
         target: 'http://a0830433.xsph.ru',
@@ -23,5 +12,11 @@ export default defineConfig({
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
     },
-  },
-});
+  } : undefined,
+  define: {
+    'process.env.NODE_ENV': JSON.stringify(command === 'serve' ? 'development' : 'production'),
+    'process.env.API_URL': command === 'serve'
+      ? JSON.stringify('/api')
+      : JSON.stringify('http://a0830433.xsph.ru')
+  }
+}))
